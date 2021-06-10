ec6aab9f50d1385a93ee2c8fd35c12651ea64e97
jasmin1109 <rankjasmin12@gmail.com>
2021-06-10 11:25:58 +0530

    init

---
+++ src/Home.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import web from "../src/images/image1.jpg";
+
+
+const Home = () => {
+    return (
+            <>
+         <section id="header" className="d-flex align-items-center">
+             <div  className="container-fluid nav_bg">
+             <div className="row">
+                 <div className="col-10 mx-auto">
+                 <div className="row">
+                 <div className="row">
+                     <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
+                        <h1>
+                            Grow your business with <strong className="brand-name">WebInfotech</strong>
+                        </h1>   
+                        <h2 className="mv-3">
+                            We are the best team of talented devloper making websites
+                        </h2>
+                        <div className="mt-3">
+                            <NavLink to="/services" className="btn-get-started">
+                                Get Started
+                            </NavLink>
+                        </div>
+                     </div>
+                    <div className="col-lg-6 order-1 order-lg-2 header-img">
+                        <img src={web} className="img-fluid animated" alt="home img" />
+                    </div>
+                    </div>
+                    </div>
+                 </div>
+             </div>
+             </div>
+            
+
+
+         </section>
+            </>
+
+
+    )
+}
+
+export default Home;
---
+++ src/Services.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const Services = () => {
+    return (
+            <>
+            <h1>Services page</h1>
+            </>
+
+
+    )
+}
+
+export default Services;
